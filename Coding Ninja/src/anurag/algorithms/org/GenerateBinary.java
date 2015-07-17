c79d9a4cf084aeb07256e9b5dc929f3c8247ca93
anuragshendge <shendge.anurag@gmail.com>
2015-07-17 17:04:52 -0400

    Add Binary number generation using queues and BFS visualization

---
+++ Coding Ninja/src/anurag/algorithms/org/GenerateBinary.java
@@ -0,0 +1,40 @@
+package anurag.algorithms.org;
+
+import java.util.*;
+
+/*A simple code for generating binary number 
+ * represetation of numbers from 1 to n
+ * given a n.
+ * 
+ * */
+public class GenerateBinary {
+
+	  Queue<String>  q = new LinkedList<>();
+
+	private void generate(int n) {
+		q.offer("1");
+		int i=1;
+		while(n!=0){
+			
+			String s1 = q.poll();
+			
+			String s2 = s1;
+			
+			System.out.println(" Number="+i+" --> "+s1);
+			q.offer(s1.concat("0"));
+			q.offer(s2.concat("1"));
+			n--;
+			i++;
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		GenerateBinary o = new GenerateBinary();
+		
+		o.generate(1000);
+
+	}
+
+}
